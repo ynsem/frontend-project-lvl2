3d8f5339bc4f4c00a53a1baaed08fe6f716ec972
ynsem <y.n.semenchenko@mail.ru>
2020-04-20 02:11:05 +0300

    промежуточный

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import fs from 'fs';
+
+// const fs = require('fs');
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.');
 
+program
+  .option('-f, --format [type]>', 'output format')
+  .arguments('<firstFileName> <secondFileName>')
+  .action((firstFileName, secondFileName) => {
+    const firstFile = fs.readFileSync(firstFileName, 'utf8');
+    const secondFile = fs.readFileSync(secondFileName, 'utf8');
+    const a = JSON.parse(firstFile);
+    const b = JSON.parse(secondFile);
+    // console.log(bb);
+    // console.log(aa);
+    // console.log(process.cwd());
+    const obj = Object.assign(a);
+    console.log(obj);
+  });
+// двойной цикл, пока ничего умнее не придумал
 program.parse(process.argv);
