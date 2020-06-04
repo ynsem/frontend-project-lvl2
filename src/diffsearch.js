e2246eaf725e86f8d3bad8bda2a8a59c2db0af8c
ynsem <y.n.semenchenko@mail.ru>
2020-06-04 03:01:20 +0300

    кажется работает

--- src/diffsearch.js
+++ src/diffsearch.js
@@ -14,55 +14,29 @@ const diff = (objBefore, objAfter) => {
     if (_.has(objAfter, item) && _.has(objBefore, item)) {
       // если оба ключа объекты
       if (_.isObject(objBefore[item]) && _.isObject(objAfter[item])) {
-        // resultArray.push(`    ${item}: ${objBefore[item]}`);
         resultArray.push({
           type: ' ',
           key: item,
-          value: objBefore[item],
+          value: diff(objBefore[item], objAfter[item]),
         });
       } else if (objBefore[item] === objAfter[item]) {
         // и его значение не менялось
-        // resultArray.push(`    ${item}: ${objBefore[item]}`);
-        resultArray.push({
-          type: ' ',
-          key: item,
-          value: objBefore[item],
-        });
+        resultArray.push({ type: ' ', key: item, value: objBefore[item] });
         // иначе, вносим с + и -
       } else {
-        // resultArray.push(`  + ${item}: ${objAfter[item]}`);
-        // resultArray.push(`  - ${item}: ${objBefore[item]}`);
-        resultArray.push({
-          type: '+',
-          key: item,
-          value: objAfter[item],
-        });
-        resultArray.push({
-          type: '-',
-          key: item,
-          value: objBefore[item],
-        });
+        resultArray.push({ type: '+', key: item, value: objAfter[item] });
+        resultArray.push({ type: '-', key: item, value: objBefore[item] });
       }
     }
 
     // если в after нет ключа из before, то вносим в resultArray с минусом
     if (!_.has(objAfter, item)) {
-      // resultArray.push(`  - ${item}: ${objBefore[item]}`);
-      resultArray.push({
-        type: '-',
-        key: item,
-        value: objBefore[item],
-      });
+      resultArray.push({ type: '-', key: item, value: objBefore[item] });
     }
 
     // если в before нет ключа из after, то вносим в resultArray с плюсом
     if (!_.has(objBefore, item)) {
-      // resultArray.push(`  + ${item}: ${objAfter[item]}`);
-      resultArray.push({
-        type: '+',
-        key: item,
-        value: objAfter[item],
-      });
+      resultArray.push({ type: '+', key: item, value: objAfter[item] });
     }
   });
 
@@ -75,10 +49,11 @@ const diffSearch = (firstConfig, secondConfig) => {
   // объект полученный преобразованием второго файла
   const objSecond = parse(secondConfig);
 
-  // const result = diff(objFirst, objSecond);
-  // return `{\n${result.join('\n')}\n}`;
+  const result = diff(objFirst, objSecond);
 
-  return diff(objFirst, objSecond);
+  console.log(result[1].value);
+  console.log('---------------------');
+  return result;
 };
 
 export default diffSearch;
